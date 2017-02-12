8a857d09072712dcf390053f86d108bec8e31787
Panthere <brynaldo8@gmail.com>
2017-02-12 12:56:27 +1030

    Start Work
    First commit, not much done on the project.

---
+++ AntiNET2/Core/Models/AssemblySettings.cs
@@ -0,0 +1,15 @@
+﻿using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiNET2.Core.Models
+{
+    public class AssemblySettings
+    {
+        public ModuleDefMD Module;
+        
+    }
+}
---
+++ AntiNET2/Core/Models/IDetectionProcess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiNET2.Core.Models
+{
+    interface IDetectionProcess
+    {
+        int Detect(AssemblySettings asm);
+    }
+}
